406ef4bf78e5fd72f82ce742b7af68cfeaca1e93
marta-probierz <marta.anna.probierz@gmail.com>
2023-05-12 12:48:55 +0200

    add interface and validation

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Field, reduxForm } from "redux-form";
+import { Field, reduxForm, reset } from "redux-form";
 import { TextField, FormControl, Select, InputLabel, FormHelperText, Box, Grid } from "@material-ui/core/";
 import Container from "@mui/material/Container";
 import { ToastContainer } from "react-toastify";
@@ -8,10 +8,9 @@ import "react-toastify/dist/ReactToastify.css";
 import i18n from "../i18n";
 
 import sendDataToApi from "../service/sendDataToApi";
-import normalizePreparationTime from "../utils/normalizePreparationTime";
 import { Footer } from "../components/Footer";
-import '../style/FormPage.style.scss';
 import { Languages } from "../components/Language";
+import '../style/FormPage.style.scss';
 
 const validate = (values) => {
   const errors = {};
@@ -25,8 +24,14 @@ const validate = (values) => {
     "slices_of_bread",
   ];
   requiredFields.forEach((field) => {
+    if (values['name'] === undefined || values['name'].length < 3) {
+      errors['name'] = i18n.t('form.validation.min');
+    }
+    if (values['spiciness_scale'] === undefined || values['spiciness_scale'] < 1 || values['spiciness_scale'] > 10) {
+      errors['spiciness_scale'] = i18n.t('form.validation.spiciness_scale');
+    }
     if (!values[field]) {
-      errors[field] = "This field is required";
+      errors[field] = i18n.t('form.validation.required');
     }
   });
   return errors;
@@ -41,6 +46,22 @@ const renderTextField = ({ label, input, meta: { touched, invalid, error }, ...c
     {...input}
     {...custom}
     style={{ minWidth: 197 }}
+  />
+);
+
+const renderTimeField = ({ label, input, meta: { touched, invalid, error }, ...custom }) => (
+  <TextField
+    label={label}
+    placeholder={label}
+    error={touched && invalid}
+    helperText={touched && error}
+    {...input}
+    {...custom}
+    style={{ minWidth: 197 }}
+    inputProps={{
+      step: 3
+    }}
+    InputLabelProps={{ shrink: true }}
   />
 );
 
@@ -83,6 +104,10 @@ const changeLanguage = (lng) => {
   i18n.changeLanguage(lng);
 };
 
+const afterSubmit = (result, dispatch) => {
+  dispatch(reset('dishes-form'))
+}
+
 const FormPage = (props) => {
   const { t } = useTranslation();
   const [selected, setSelected] = useState({ type: "" });
@@ -101,15 +126,15 @@ const FormPage = (props) => {
         <Grid container justifyContent="center" alignItems="center" style={{ height: "90vh" }}>
           <form className="dishes-form" onSubmit={handleSubmit(sendDataToApi)}>
             <div className="container-main">
-              <Box mb={1}>
+              <Box mb={2}>
                 <Field name="name" component={renderTextField} label={t`form.name`} />
               </Box>
               <Box mb={1}>
                 <Field
                   name="preparation_time"
-                  component={renderTextField}
+                  component={renderTimeField}
                   label={t`form.preparation_time`}
-                  normalize={normalizePreparationTime}
+                  type="time"
                 />
               </Box>
               <Box mb={1}>
@@ -198,4 +223,5 @@ const FormPage = (props) => {
 export default reduxForm({
   form: "dishes-form",
   validate,
+  onSubmitSuccess: afterSubmit,
 })(FormPage);
